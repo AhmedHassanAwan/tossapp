0de054c75e877edee0236591744a24191e01da4b
AhmedHasssanAwan <awanhasan2007@gmail.com>
2024-10-01 03:06:49 -0700

    Toss-App

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let head = document.querySelector("#btn1")
+let tails = document.querySelector("#btn2")
+let result = document.querySelector("#result")
+let img = document.querySelector("#img")
+let num;
+function toss(para) {
+    num = Math.ceil(Math.random(num) * 2)
+    if (para === 1 && num === 1 || para === 2 && num === 2) {
+        result.innerHTML = "The Winner Is Heads!"
+        console.log("You Win The Toss!" + num);
+        
+    } else {
+        console.log("You Loss The Toss!" + num);
+        result.innerHTML = "The Winner Is Tails!"
+    }
+    if (num === 1) {
+        img.src = "image./head.png"
+        
+    } else if (num === 2) {
+        img.src = "image./tails.png"
+    
+    }
+}
